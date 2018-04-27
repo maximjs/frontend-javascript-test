3c968c2039b1e6373cfca33a258934ae7aa59e48
Kozlyakov Maxim <maxim.kozlyakov@gmail.com>
2018-04-28 02:39:38 +0300

    deploy on heroku #2

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,7 +5,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 export default () => ({
   devtool: 'inline-source-map',
   entry: {
-    app: ['./app/index.js'],
+    app: ['./app'],
   },
   output: {
     library: 'WebpackPackage',
